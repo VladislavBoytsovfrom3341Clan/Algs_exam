5133419d9cc16d3ef26b9852c91740d5a7c5e2aa
VladislavBoytsovfrom3341Clan <vladybout@gmail.com>
2025-01-25 18:37:03 +0300

    Добавьте файлы проекта.

---
+++ Algs_exam/Algs_exam.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+#include "SinglyLinkedList.h"
+
+int main()
+{
+    SinglyLinkedList<int> my_list;
+    my_list.push_back(5);
+    
+}
+
---
+++ Algs_exam/SinglyLinkedList.h
@@ -0,0 +1,211 @@
+#pragma once
+
+#include <stdexcept>
+
+template<typename T>
+struct SinglyLinkedListNode
+{
+	T data;
+	SinglyLinkedListNode* next = nullptr;
+
+	SinglyLinkedListNode();
+
+	SinglyLinkedListNode(T val, SinglyLinkedListNode* ptr = nullptr);
+};
+
+template<typename T>
+class SinglyLinkedList
+{
+	SinglyLinkedListNode<T>* m_head = nullptr;
+	SinglyLinkedListNode<T>* m_tail = nullptr;
+	size_t m_size = 0;
+
+public:
+
+	SinglyLinkedList();
+
+	SinglyLinkedList(T val);
+
+	bool empty();
+
+	int size();
+
+	void push_front(T val);
+
+	void push_back(T val);
+
+	void pop_front();
+
+	void pop_back();
+
+	T& front();
+
+	T& back();
+
+	T& operator[] (int n);
+
+	const T& operator[] (int n) const;
+
+	~SinglyLinkedList();
+};
+
+template<typename T>
+inline SinglyLinkedListNode<T>::SinglyLinkedListNode()
+{
+	next = nullptr;
+}
+
+template<typename T>
+inline SinglyLinkedListNode<T>::SinglyLinkedListNode(T val, SinglyLinkedListNode* ptr):
+	data(val), next(ptr)
+{
+}
+
+template<typename T>
+inline SinglyLinkedList<T>::SinglyLinkedList()
+{
+	m_head = nullptr;
+	m_tail = nullptr;
+	m_size = 0;
+}
+
+template<typename T>
+inline SinglyLinkedList<T>::SinglyLinkedList(T val)
+{
+	m_head = new SinglyLinkedListNode<T>(val);
+	m_tail = m_head;
+	m_size = 1;
+}
+
+template<typename T>
+inline bool SinglyLinkedList<T>::empty()
+{
+	return(m_head == nullptr);
+}
+
+template<typename T>
+inline int SinglyLinkedList<T>::size()
+{
+	return m_size;
+}
+
+template<typename T>
+inline void SinglyLinkedList<T>::push_front(T val)
+{
+	SinglyLinkedListNode<T>* new_head = new SinglyLinkedListNode<T>(val, m_head);
+	if (m_tail == nullptr)
+		m_tail = new_head;
+	m_head = new_head;
+	m_size++;
+}
+
+template<typename T>
+inline void SinglyLinkedList<T>::push_back(T val)
+{
+	SinglyLinkedListNode<T>* new_tail = new SinglyLinkedListNode<T>(val);
+	if (m_tail == nullptr)
+	{
+		m_tail = new_tail;
+		m_head = m_tail;
+	}
+	else
+	{
+		m_tail->next = new_tail;
+		m_tail = new_tail;
+	}
+	m_size++;
+}
+
+template<typename T>
+inline void SinglyLinkedList<T>::pop_front()
+{
+	if (m_head == nullptr)
+		return;
+	SinglyLinkedListNode<T>* next = m_head->next;
+	delete m_head;
+	m_head = next;
+	if (m_head == nullptr)
+		m_tail = nullptr;
+	m_size--;
+}
+
+template<typename T>
+inline void SinglyLinkedList<T>::pop_back()
+{
+	if (m_tail == nullptr)
+		return;
+	SinglyLinkedListNode<T>* prev_tail = m_head;
+	if (prev_tail == m_tail)
+	{
+		delete prev_tail;
+		m_head = nullptr;
+		m_tail = nullptr;
+	}
+	else
+	{
+		while (prev_tail->next != m_tail)
+			prev_tail = prev_tail->next;
+		prev_tail->next = nullptr;
+		delete m_tail;
+		m_tail = prev_tail;
+	}
+	m_size--;
+}
+
+template<typename T>
+inline T& SinglyLinkedList<T>::front()
+{
+	if (m_head == nullptr)
+		throw std::runtime_error("Trying to access empty list element\n");
+	return m_head->data;
+}
+
+template<typename T>
+inline T& SinglyLinkedList<T>::back()
+{
+	if (m_tail == nullptr)
+		throw std::runtime_error("Trying to access empty list element\n");
+	return m_tail->data;
+}
+
+template<typename T>
+inline T& SinglyLinkedList<T>::operator[](int n)
+{
+	if (m_head == nullptr)
+		throw std::runtime_error("Trying to access empty list element\n");
+	SinglyLinkedListNode<T>* ptr = m_head;
+	for (n; n > 0; n--)
+	{
+		ptr = ptr->next;
+		if (ptr == nullptr)
+			throw std::runtime_error("Invalid list element index\n");
+	}
+	return ptr->data;
+}
+
+template<typename T>
+inline const T& SinglyLinkedList<T>::operator[](int n) const
+{
+	if (m_head == nullptr)
+		throw std::runtime_error("Trying to access empty list element\n");
+	const SinglyLinkedListNode<T>* ptr= m_head;
+	for (n; n > 0; n--)
+	{
+		ptr = ptr->next;
+		if (ptr == nullptr)
+			throw std::runtime_error("Invalid list element index\n");
+	}
+	return ptr->data;
+}
+
+template<typename T>
+inline SinglyLinkedList<T>::~SinglyLinkedList()
+{
+	while (m_head != nullptr)
+	{
+		SinglyLinkedListNode<T>* next = m_head->next;
+		delete m_head;
+		m_head = next;
+	}
+}
+
